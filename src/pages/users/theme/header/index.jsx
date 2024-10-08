1459a3b81498cb0e891c43a03714bc7f50f39eb5
thinhleduc387 <162032314+thinhleduc387@users.noreply.github.com>
2024-10-08 23:27:16 +0700

    Navbar

--- src/pages/users/theme/header/index.jsx
+++ src/pages/users/theme/header/index.jsx
@@ -1,57 +1,32 @@
 import { memo } from "react";
 //import "./style.scss";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.scss";
 
 const Header = () => {
   const expand = "lg";
 
   return (
-    <Navbar expand={expand} className="bg-body-tertiary mb-3">
+    <Navbar expand={expand} className="mb-3">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-        <Navbar.Offcanvas
-          id={`offcanvasNavbar-expand-${expand}`}
-          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-          placement="end"
-        >
+        <Navbar.Brand href="#">AppleHouse</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Offcanvas placement="end">
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-              Offcanvas
-            </Offcanvas.Title>
+            <Offcanvas.Title>AppleHouse</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-              </Form>
+            <Form className="d-flex search-field">
+              <Form.Control type="search" placeholder="Search" />
+            </Form>
+            <Nav className="justify-content-start flex-grow-1">
               <Nav.Link href="#action1">Home</Nav.Link>
               <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown
-                title="Dropdown"
-                id={`offcanvasNavbarDropdown-expand-${expand}`}
-              >
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
