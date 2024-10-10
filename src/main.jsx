71f2c4456369925f2f3ea859ab1d95b803de76f3
thinhleduc387 <162032314+thinhleduc387@users.noreply.github.com>
2024-10-11 02:51:43 +0700

    tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import RouterCustom from "./router";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './style/style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
