4ac143009881e31c57f57d8d70165e95064e7874
thinhleduc387 <162032314+thinhleduc387@users.noreply.github.com>
2024-12-14 11:41:51 +0700

    comment

--- src/component/Product/Feedback/Comment.jsx
+++ src/component/Product/Feedback/Comment.jsx
@@ -1,16 +1,25 @@
-import CommentItem from './CommentItem';
+import CommentItem from "./CommentItem";
+import CommentReply from "./CommentReply";
 
-const Comment = () => {
+const Comment = ({ commentIndex, replyIndex, onReplyClick, onCloseInput, onSendComment }) => {
+  const isReplying = replyIndex === commentIndex; // Kiểm tra xem comment có đang được mở input không
+
   return (
     <div>
       <div>
-        <CommentItem />
+        {/* Bình luận đầu tiên */}
+        <CommentItem isFirst={true} onReplyClick={() => onReplyClick(commentIndex)} />
+        {isReplying && (
+          <div className="ml-12 mt-4">
+            <CommentReply onClose={onCloseInput} onSend={onSendComment} />
+          </div>
+        )}
       </div>
       <div className="bg-slate-400 ml-12 px-4 py-6 mt-4 space-y-6 rounded-lg">
-        <CommentItem />
-        <CommentItem />
+        {/* Các bình luận còn lại */}
+        <CommentItem isFirst={false} />
+        <CommentItem isFirst={false} />
       </div>
-
     </div>
   );
 };
