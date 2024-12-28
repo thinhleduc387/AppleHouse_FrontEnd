a13b8675ba49f4233770f0627d1c7fb9958ad7b7
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2024-12-29 01:41:06 +0700

    fix

--- src/layout/userLayout/index.jsx
+++ src/layout/userLayout/index.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 const MasterLayout = ({ ...props }) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="w-full fixed top-0 left-0 right-0 z-9 bg-white">
+      <div className="w-full fixed top-0 left-0 right-0 z-50 bg-white">
         <Header />
       </div>
 
